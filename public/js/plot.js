081dbca95cb3fc05e0dd46605ea7e3f47b8706ca
Fanny <huang.xingfan@gmail.com>
2022-06-02 23:11:42 -0700

    working barplots

--- public/js/plot.js
+++ public/js/plot.js
@@ -2,31 +2,53 @@ import { colors } from './color.js';
 import { genes } from './genes.js';
 import { motifs } from './motifs.js';
 
+function calculateAverage(params) {
+    var sum = 0;
+    var count = 0;
+    for (var i=0; i<params.length; i++) {
+        //if (params[i] !== 0) {
+            sum = sum + Number(params[i]);
+            count++;
+       // }
+    }
+    return sum / count;
+}
 
 export default class Plot {
   plot;
   plot_id;
+  bar_plot;
+  bar_plot_id;
   timepoint_selector;
   annotation_selector;
+  groupby_selector;
   gene_selector;
   legend_toggle;
   detail = "annotation";
   traces = [];
   colors = {};
+  bar_data = {};
   legendVisible = true;
 
-  constructor(plot_id, timepoint_id, annotation_id, gene_id, gene_w_id, legend_id, loading_id, apply_id, title_id, assay) {
+  constructor(plot_id, timepoint_id, annotation_id, gene_id, gene_w_id, legend_id, loading_id, apply_id, title_id, assay, bar_plot_id,bar_loading_id, bar_apply_id, groupby_id, groupby_wrapper, apply_wrapper) {
     this.plot_id = plot_id;
+    this.bar_plot_id = bar_plot_id;
     this.timepoint_selector = document.getElementById(timepoint_id);
     this.annotation_selector = document.getElementById(annotation_id);
+    this.groupby_selector = document.getElementById(groupby_id);
     this.gene_selector = document.getElementById(gene_id);
     this.gene_wrapper = document.getElementById(gene_w_id);
+    this.groupby_wrapper = document.getElementById(groupby_wrapper);
+    this.apply_wrapper = document.getElementById(apply_wrapper);
     this.legend_toggle = document.getElementById(legend_id);
     this.colors = colors;
     this.loading_div = document.getElementById(loading_id);
+    this.bar_loading_div = document.getElementById(bar_loading_id);
     this.apply = document.getElementById(apply_id);
+    this.bar_apply = document.getElementById(bar_apply_id);
     this.title = document.getElementById(title_id);
     this.assay = assay;
+    this.bar_layout = { yaxis: { title: 'Counts' } };
     console.log('finished construction');
   }
 
@@ -38,12 +60,20 @@ export default class Plot {
     return this.plot;
   }
 
+  get bar_plot_id() {
+    return this.bar_plot_id;
+  }
+
+  get bar_plot() {
+    return this.bar_plot;
+  }
+  
   async create_plot() {
     this.#update_title();
     console.log('creating plot');
     let traces = await this.#generate_traces();
+    let datas = await this.#generate_bar_data();
     console.log('done generating traces')
-
     let layout = {
       scene: {
         xaxis: {
@@ -64,24 +94,44 @@ export default class Plot {
     };
 
     await Plotly.newPlot(this.plot_id, traces, layout, { responsive: true })
+    this.#update_bar_layout();
+    await Plotly.newPlot(this.bar_plot_id, [datas], this.bar_layout)
     this.loading_div.classList.remove('lds-ellipsis');
+    this.bar_loading_div.classList.remove('lds-ellipsis');
     this.plot = document.getElementById(this.plot_id);
+    this.bar_plot = document.getElementById(this.bar_plot_id);
     this.#add_listeners();
   }
 
-  async update_plot() {
-    console.log('updating');
-    let old_traces_length = this.traces.length;
-    try {
-      await this.#generate_traces();
-    } catch (err) {
-      document.getElementById("error-message").innerHTML = err;
-      return
+  async update_plot(update_all=true) {
+    if (update_all) {
+      console.log('updating');
+      let old_traces_length = this.traces.length;
+      try {
+        await this.#generate_traces();
+        await this.#generate_bar_data();
+      } catch (err) {
+        document.getElementById("error-message").innerHTML = err;
+        return;
+      }
+      Plotly.deleteTraces(this.plot_id, [...Array(old_traces_length).keys()]);
+      this.#update_title();
+      Plotly.addTraces(this.plot_id, [...this.traces]);
+      this.#update_bar_layout();
+      Plotly.react(this.bar_plot_id, [this.bar_data], this.bar_layout);
+      this.loading_div.classList.remove('lds-ellipsis');
+      this.bar_loading_div.classList.remove('lds-ellipsis');
+    } else {
+      try {
+        await this.#generate_bar_data();
+      } catch (err) {
+        document.getElementById("error-message").innerHTML = err;
+        return;
+      }
+      this.#update_bar_layout();
+      Plotly.react(this.bar_plot_id, [this.bar_data], this.bar_layout);
+      this.bar_loading_div.classList.remove('lds-ellipsis');
     }
-    Plotly.deleteTraces(this.plot_id, [...Array(old_traces_length).keys()]);
-    this.#update_title();
-    Plotly.addTraces(this.plot_id, [...this.traces]);
-    this.loading_div.classList.remove('lds-ellipsis');
   }
 
   async #generate_traces() {
@@ -93,13 +143,11 @@ export default class Plot {
       this.loading_div.classList.remove('lds-ellipsis');
       throw err
     }
-    console.log(data);
     let filtered_data = [];
     let traces = [];
     let annotation = this.annotation_selector.value;
     let unique = [];
     let color = [];
-
 
     if (this.detail == "gene") {
       unique = ['expression'];
@@ -136,6 +184,72 @@ export default class Plot {
     this.traces = traces;
     return traces;
   }
+  
+  async #generate_bar_data() { //TODO:merge with generate_traces
+    this.bar_loading_div.classList.add('lds-ellipsis');
+    let data;
+    try {
+      data = await this.#fetch_data();
+    } catch (err) {
+      this.bar_loading_div.classList.remove('lds-ellipsis');
+      throw err
+    }
+    let filtered_data = [];
+    let bar_x = [];
+    let bar_y = [];
+    let annotation = this.annotation_selector.value;
+    let groupby = this.groupby_selector.value;
+    let unique = [];
+    let color = [];
+
+    if (this.detail == "gene") {
+      unique = ['expression'];
+    } else if (annotation == 'inferred_time') {
+      unique = ['inferred_time'];
+    } else {
+      unique = [...new Set(this.#unpack(data, annotation))];
+    }
+
+    for (let item in unique) {
+      if (annotation != 'inferred_time' & this.detail != 'gene') {
+        filtered_data = data.filter((el) => el[annotation] == unique[item]);
+        color = this.colors[unique[item]];
+        bar_x.push(unique[item]); 
+        bar_y.push(filtered_data.length);
+      } else {
+        filtered_data = data;
+        color = this.#unpack(filtered_data, unique[item]);
+        let groupby_data;
+        try {
+          groupby_data = await this.#fetch_bar_data();
+        } catch (err) {
+          this.bar_loading_div.classList.remove('lds-ellipsis');
+          throw err
+        }
+        let groupby_array = this.#unpack(groupby_data, groupby)
+        var map = new Map();
+        for (var i=0; i < groupby_array.length; i++) {
+            var key = groupby_array[i];
+            var value = color[i];
+            if(!map.has(key)){
+              map.set(key, [value]);
+            } else {
+              var old = map.get(key);
+              old.push(value);
+            }
+        }
+        map.forEach((values,keys) => {
+          var avg = calculateAverage(values);
+          bar_x.push(keys); 
+          bar_y.push(avg);
+          console.log("keys: " + keys + " avg: " + avg);
+        })
+      }
+    }
+    
+    this.bar_data = {x:bar_x, y:bar_y, type: 'bar'};
+    return {x:bar_x, y:bar_y, type: 'bar'};
+  }
 
   async #fetch_data() {
     if (this.detail == 'gene' & (!this.gene_selector.value || !genes.includes(this.gene_selector.value)) & this.assay=='RNA') {
@@ -158,6 +272,22 @@ export default class Plot {
     }
     return res.json();
   }
+  
+  async #fetch_bar_data() {
+    let params = {
+      "timepoint": this.timepoint_selector.value,
+      "annotation": this.groupby_selector.value,
+      "gene": '',
+      "assay": this.assay
+    }
+
+    console.log('fetching data');
+    const res = await fetch('/deap/data?' + new URLSearchParams(params));
+    if (!res.ok) {
+      throw "Database Error"
+    }
+    return res.json();
+  }
 
   #add_listeners() {
     this.annotation_selector.onchange = () => {
@@ -166,12 +296,24 @@ export default class Plot {
         $(this.gene_wrapper).addClass('slide-right')
         this.detail = "gene"
         $(this.apply).addClass("button-primary");
+        $(this.groupby_wrapper).removeClass('hide');
+        $(this.apply_wrapper).removeClass('hide');
       } else {
         this.detail = "annotation";
         $(this.gene_wrapper).addClass('hide')
         $(this.gene_wrapper).removeClass('slide-right')
         $(this.apply).addClass("button-primary");
+        $(this.groupby_wrapper).addClass('hide');
+        $(this.apply_wrapper).addClass('hide');
+      };
+      if (this.annotation_selector.value == 'inferred_time') {
+        $(this.groupby_wrapper).removeClass('hide');
+        $(this.apply_wrapper).removeClass('hide');
       }
+    };
+      
+    this.groupby_selector.onchange = () => {
+      $(this.bar_apply).addClass("button-primary");
     };
 
     this.gene_selector.oninput = () => this.#generate_genes_field_autocomplete();
@@ -186,6 +328,12 @@ export default class Plot {
       document.getElementById("error-message").innerHTML = "";
       this.update_plot();
       $(this.apply).removeClass("button-primary");
+    });
+    
+    this.bar_apply.addEventListener('click', () => {
+      document.getElementById("error-message").innerHTML = "";
+      this.update_plot(false);
+      $(this.bar_apply).removeClass("button-primary");
     });
   }
 
@@ -250,5 +398,19 @@ export default class Plot {
     this.title.innerHTML = `sc${this.assay} cells in ${this.timepoint_selector.value} colored by ${colored_by}`
   }
 
+  #update_bar_layout() {
+    let yaxis_text = 'Counts'
+    if (this.detail == 'gene') {
+      if (this.assay == 'RNA') {
+        yaxis_text = `Mean of ${this.gene_selector.value} gene expression`
+      } else {
+        yaxis_text = `Mean of ${this.gene_selector.value} motif activity z-scores`
+      }
+    } else if (this.annotation_selector.value == 'inferred_time') {
+      yaxis_text = `Mean of inferred time`
+    }
+    this.bar_layout = { yaxis: { title: yaxis_text } };
+  }
+  
   #unpack(data, key) { return data.map(row => row[key]) }
 }
--- public/js/scatter_plots.js
+++ public/js/scatter_plots.js
@@ -1,7 +1,7 @@
 import Plot from './plot.js';
 
-const plot1 = new Plot('scatter-plot-1', 'timepoint', 'annotation_1', 'gene_1', 'gene_wrapper_1', 'legend-1', 'loading-1', 'apply-1', 'title_1', 'RNA');
-const plot2 = new Plot('scatter-plot-2', 'timepoint', 'annotation_2', 'gene_2', 'gene_wrapper_2', 'legend-2', 'loading-2', 'apply-2', 'title_2', 'ATAC');
+const plot1 = new Plot('scatter-plot-1', 'timepoint', 'annotation_1', 'gene_1', 'gene_wrapper_1', 'legend-1', 'loading-1', 'apply-1', 'title_1', 'RNA', 'bar-plot-1', 'loading-3','apply-3','groupby_1','groupby_wrapper_1','apply_wrapper_1');
+const plot2 = new Plot('scatter-plot-2', 'timepoint', 'annotation_2', 'gene_2', 'gene_wrapper_2', 'legend-2', 'loading-2', 'apply-2', 'title_2', 'ATAC', 'bar-plot-2', 'loading-4','apply-4','groupby_2','groupby_wrapper_2','apply_wrapper_2');
 
 Promise.all([
   plot1.create_plot(),
